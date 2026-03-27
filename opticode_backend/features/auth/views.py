69dbc28a6c38bbeafc68cdef4378f33fcf61d53b
Juan Manuel Chavez Arredondo <20233tn193@utez.edu.mx>
2026-03-26 22:37:39 -0600

    [FEAT] agregar endpoint de JWT en login
    Co-authored-by: 20233tn193 <20233tn193@utex.edu.mx>

--- opticode_backend/features/auth/views.py
+++ opticode_backend/features/auth/views.py
@@ -1,9 +1,32 @@
-from rest_framework import generics
+from rest_framework import generics, status, views
 from rest_framework.permissions import AllowAny
-
-from .serializers import RegisterSerializer
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import RegisterSerializer, LoginSerializer
+from django.contrib.auth import authenticate
 
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
     permission_classes = [AllowAny]
+
+class LoginView(views.APIView):
+    permission_classes = [AllowAny]
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            email = serializer.validated_data['email']
+            password = serializer.validated_data['password']
+            user = authenticate(request, username=email, password=password)
+
+            if user is not None:
+                refresh = RefreshToken.for_user(user)
+                return Response({
+                    'refresh': str(refresh),
+                    'access': str(refresh.access_token),
+                }, status=status.HTTP_200_OK)
+            else:
+                return Response({'error': 'Credenciales inválidas'}, status=status.HTTP_401_UNAUTHORIZED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
