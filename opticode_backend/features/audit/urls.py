de1ba45266206b4a522488236d7a3b80e5324a7d
Loreley100 <20233tn227@utez.edu.mx>
2026-03-30 23:55:02 -0600

    [FEAT] crear endpoint para exportar reporte a PDF
    Creación de endpoint GET en audit/views.py para generar reportes en PDF usando WeasyPrint. Actualización de rutas

--- opticode_backend/features/audit/urls.py
+++ opticode_backend/features/audit/urls.py
@@ -1,5 +1,8 @@
-from django.urls import path  # noqa: F401
+from django.urls import path
+from .views import AuditReportPDFView
 
 app_name = "audit"
 
-urlpatterns = []
+urlpatterns = [
+    path('<str:fileId>/report/pdf/', AuditReportPDFView.as_view(), name='audit-report-pdf'),
+]
