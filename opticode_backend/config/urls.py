e13ca4a689b170dc3476c9e493d537d52038355d
20233tn193 <20233tn193@utex.edu.mx>
2026-03-31 13:06:33 -0600

    [FEAT] Agregar ruta /api/token/refresh/ para renovación de JWT

--- opticode_backend/config/urls.py
+++ opticode_backend/config/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/auth/", include("features.auth.urls")),
     path("api/users/", include("features.users.urls")),
     path("api/projects/", include("features.projects.urls")),
