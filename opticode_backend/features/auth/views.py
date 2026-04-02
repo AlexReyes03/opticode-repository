df61f5084e39482b04494c30baa5e75166c28af5
20233tn193 <20233tn193@utex.edu.mx>
2026-04-01 20:06:29 -0600

    [FEAT] Agregar enpoint get usuario actual

--- opticode_backend/features/auth/views.py
+++ opticode_backend/features/auth/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics, status, views
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from .serializers import RegisterSerializer, LoginSerializer
@@ -30,3 +30,21 @@ class LoginView(views.APIView):
             else:
                 return Response({'error': 'Credenciales inválidas'}, status=status.HTTP_401_UNAUTHORIZED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class MeView(views.APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        return Response(
+            {
+                'id': user.id,
+                'email': user.email,
+                'first_name': user.first_name,
+                'last_name': user.last_name,
+                'is_staff': user.is_staff,
+                'date_joined': user.date_joined,
+            },
+            status=status.HTTP_200_OK,
+        )
