82d6f4c1a50d7c5cd3494c41c90a6491a799cbb9
Jafetvu <20233tn169@utez.edu.mx>
2026-04-13 10:16:21 -0600

    [FEAT] agregar campos audito-relacionales a UploadedFile para completar las auditorías

--- opticode_backend/features/audit/models.py
+++ opticode_backend/features/audit/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 
 class UploadedFile(models.Model):
@@ -18,6 +19,18 @@ class UploadedFile(models.Model):
     score = models.FloatField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    # Campos de Tabla Auditable
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.SET_NULL, null=True, blank=True, 
+        related_name='created_files', verbose_name="creado por"
+    )
+    updated_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.SET_NULL, null=True, blank=True, 
+        related_name='updated_files', verbose_name="actualizado por"
+    )
 
     class Meta:
         db_table = "uploaded_files"
